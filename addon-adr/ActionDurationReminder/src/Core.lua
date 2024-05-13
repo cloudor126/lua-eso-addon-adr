600d694671c2c93bde3d21a7be4c87e4e2038407
Cloudor <cloudor@126.com>
2024-05-13 20:20:21 +0800

    v3.116

--- addon-adr/ActionDurationReminder/src/Core.lua
+++ addon-adr/ActionDurationReminder/src/Core.lua
@@ -570,7 +570,7 @@ l.onEffectChanged -- #(#number:eventCode,#number:changeType,#number:effectSlot,#
     l.debug(DS_ACTION,1)('[] '..effectName..' ignored by id:'..abilityId..', reason:'..l.ignoredIds[abilityId])
     return
   end
-  local key =(changeType == EFFECT_RESULT_UPDATED) and  ('%d:%s:update'):format(abilityId,effectName) or ('%d:%s:%d:%d'):format(abilityId,effectName,changeType,stackCount)
+  local key =(changeType == EFFECT_RESULT_UPDATED) and  ('%d:%s:%d:update'):format(abilityId,effectName,stackCount) or ('%d:%s:%d:%d'):format(abilityId,effectName,changeType,stackCount)
   local numMarks = l.ignoredCache:get(key)
   --  df(' |t24:24:%s|t%s (id: %d) mark: %d',iconName, effectName,abilityId,numMarks)
   if numMarks>=4 then
