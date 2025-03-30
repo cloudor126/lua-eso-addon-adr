389e3619c978642784f454181a7b63bc18e3bcc0
cloudor <cloudor@126.com>
2025-03-30 21:55:12 +0800

    v3.131

--- addon-adr/ActionDurationReminder/src/Models.lua
+++ addon-adr/ActionDurationReminder/src/Models.lua
@@ -105,7 +105,7 @@ m.newAbility -- #(#number:id, #string:name, #string:icon)->(#Ability)
     ability.progressionName = nil
   end -- only keep different name
   if hasProgression and progressionIndex then
-    local _,icon3,_ = GetAbilityProgressionAbilityInfo(progressionIndex,1,1)
+    local _,icon3,_ = GetAbilityProgressionAbilityInfo(progressionIndex,0,1)
     if icon3 ~= icon then
       ability.icon3 = icon3 -- #string
     end
